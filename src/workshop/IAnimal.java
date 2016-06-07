d25dd71f692fcdd274a4c94edf27e45fbdea2535
Artit Kiuwilai <first087@gmail.com>
2016-06-07 14:56:58 +0700

    Answer Workshop 6 - Add getter/setter to interface IAnimal

--- src/workshop/IAnimal.java
+++ src/workshop/IAnimal.java
@@ -4,4 +4,10 @@ package workshop;
  * Created by Artit on 7/6/2559.
  */
 public interface IAnimal {
+    String getName();
+    void setName(String name);
+    int getAge();
+    void setAge(int age);
+    boolean isCanFly();
+    void setCanFly(boolean canFly);
 }

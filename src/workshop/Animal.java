62dc1087d77093bfabea09ec048509b61d40b813
Artit Kiuwilai <first087@gmail.com>
2016-06-08 09:19:34 +0700

    Answer Workshop 6 - Implement Animal with IAnimal

--- src/workshop/Animal.java
+++ src/workshop/Animal.java
@@ -3,5 +3,39 @@ package workshop;
 /**
  * Created by Artit on 8/6/2559.
  */
-public class Animal {
+public class Animal implements IAnimal {
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void setName(String name) {
+
+    }
+
+    @Override
+    public int getAge() {
+        return 0;
+    }
+
+    @Override
+    public void setAge(int age) {
+
+    }
+
+    @Override
+    public boolean isCanFly() {
+        return false;
+    }
+
+    @Override
+    public void setCanFly(boolean canFly) {
+
+    }
+
+    @Override
+    public String Speak() {
+        return null;
+    }
 }

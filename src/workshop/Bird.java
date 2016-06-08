e87b6dfbd0d4d10f7f0ff489300e18c101c81298
Artit Kiuwilai <first087@gmail.com>
2016-06-08 10:49:50 +0700

    Answer Workshop 7 - Remove all attribute, method speak and code in constructor in 3 classes

--- src/workshop/Bird.java
+++ src/workshop/Bird.java
@@ -4,16 +4,8 @@ package workshop;
  * Created by Artit on 8/6/2559.
  */
 public class Bird {
-    private String color;
-    private int legs;
-
     public Bird(String color, int legs) {
-        this.color = color;
-        this.legs = legs;
-    }
 
-    public String speak() {
-        return "Jib Jib";
     }
 
     public void fly() {
--- src/workshop/Dog.java
+++ src/workshop/Dog.java
@@ -4,16 +4,8 @@ package workshop;
  * Created by Artit on 8/6/2559.
  */
 public class Dog {
-    private String color;
-    private int legs;
-
     public Dog(String color, int legs) {
-        this.color = color;
-        this.legs = legs;
-    }
 
-    public String speak() {
-        return "Box Box";
     }
 
     public void run() {
